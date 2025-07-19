9dfa1abf4b67cdb97a7a1476b38cf89eb1f92a7e
Jkerdley <jk.erdley@gmail.com>
2025-07-20 01:35:12 +0700

    feat: rewrite auth on mobix and mobx store

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,28 +4,26 @@ import { Todos } from "./components/todos/Todos";
 import { NotFound } from "./components/not-found/NotFound";
 import { ProtectedRoutes } from "./ProtectedRoutes";
 import { useEffect } from "react";
-import { useAppDispatch } from "./ducks/hooks";
-import { checkAuth } from "./ducks/auth";
+import { authStore } from "./store/authStore";
 
 function App() {
-    const dispatch = useAppDispatch();
-    useEffect(() => {
-        dispatch(checkAuth());
-    }, []);
+  useEffect(() => {
+    authStore.checkAuth();
+  }, []);
 
-    return (
-        <div className="App">
-            <Routes>
-                <Route element={<ProtectedRoutes />}>
-                    <Route path="/" element={<Login />} />
-                </Route>
-                <Route element={<ProtectedRoutes auth={true} />}>
-                    <Route path="/todos" element={<Todos />} />
-                </Route>
-                <Route path="*" element={<NotFound />} />
-            </Routes>
-        </div>
-    );
+  return (
+    <div className="App">
+      <Routes>
+        <Route element={<ProtectedRoutes />}>
+          <Route path="/" element={<Login />} />
+        </Route>
+        <Route element={<ProtectedRoutes auth={true} />}>
+          <Route path="/todos" element={<Todos />} />
+        </Route>
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
--- frontend/src/components/auth/Login.tsx
+++ frontend/src/components/auth/Login.tsx
@@ -1,53 +1,51 @@
 import { FormEventHandler } from "react";
-import { useAppDispatch, useAppSelector } from "../../ducks/hooks";
-import {login as loginAction} from '../../ducks/auth'
+import { authStore } from "../../store/authStore";
 
 export const Login = () => {
-    const err = useAppSelector(store => store.auth.error)
-    const dispatch = useAppDispatch()
+  const error = authStore.error;
 
-    const onLogin: FormEventHandler<HTMLFormElement> = (e) => {
-        e.preventDefault();
+  const onLogin: FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
 
-        const form = e.target as HTMLFormElement;
+    const form = e.target as HTMLFormElement;
 
-        if (!form) {
-            return;
-        }
-
-        const formData = new FormData(form);
-
-        const login = formData.get('login');
-        const password = formData.get('password');
-
-        if (!login || !password) {
-            return;
-        }
-
-        dispatch(loginAction({
-            login: login.toString(),
-            password: password.toString()
-        }))
+    if (!form) {
+      return;
     }
 
-    return (
-        <div className="auth">
-            <div className="auth__container">
-                <h1>ToDo List</h1>
-                <form className="auth__form" onSubmit={onLogin}>
-                    <label htmlFor="login" />
-                    <input type="text" placeholder="Login" id="login" name="login" />
-                    <label htmlFor="password" />
-                    <input
-                        type="password"
-                        placeholder="Password"
-                        id="password"
-                        name="password"
-                    />
-                    <button type="submit">Log In</button>
-                    <p className="errors">{err}</p>
-                </form>
-            </div>
-        </div>
-    );
-}
+    const formData = new FormData(form);
+
+    const login = formData.get("login");
+    const password = formData.get("password");
+
+    if (!login || !password) {
+      return;
+    }
+
+    authStore.login({
+      login: login.toString(),
+      password: password.toString(),
+    });
+  };
+
+  return (
+    <div className="auth">
+      <div className="auth__container">
+        <h1>ToDo List</h1>
+        <form className="auth__form" onSubmit={onLogin}>
+          <label htmlFor="login" />
+          <input type="text" placeholder="Login" id="login" name="login" />
+          <label htmlFor="password" />
+          <input
+            type="password"
+            placeholder="Password"
+            id="password"
+            name="password"
+          />
+          <button type="submit">Log In</button>
+          <p className="errors">{error}</p>
+        </form>
+      </div>
+    </div>
+  );
+};
