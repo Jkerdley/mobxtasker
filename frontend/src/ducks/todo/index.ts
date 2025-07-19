650e292008add3cb54414a59f12eb6edfe6be49b
Jkerdley <jk.erdley@gmail.com>
2025-07-20 00:28:55 +0700

    chore: initial commit

---
+++ frontend/src/ducks/todo/index.ts
@@ -0,0 +1,8 @@
+import { todoSlice } from "./slice";
+
+export const todoReducer = todoSlice.reducer;
+export const todoReducerPath = todoSlice.reducerPath;
+export const todoMiddleware = todoSlice.middleware;
+
+export const { useGetTodosQuery, useCreateTodoMutation, useDeleteTodoMutation, useEditTodoMutation } =
+    todoSlice;
