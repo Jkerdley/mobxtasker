0c198977f5bed0b3ac2004b998f95b839c8cdc6a
Jkerdley <jk.erdley@gmail.com>
2025-07-20 02:34:05 +0700

    refactor: adds CRUD operations by mobx and delete redux toolkit

--- frontend/src/components/todos/Todo.tsx
+++ frontend/src/components/todos/Todo.tsx
@@ -1,44 +1,50 @@
-import { useEditTodoMutation } from "../../ducks/todo";
+import { observer } from "mobx-react-lite";
+import { todoStore } from "../../store/todoStore";
 import { ITodo } from "../../types/todo";
 import { DeleteTodo } from "./DeleteTodo";
 
 interface ITodoProps {
-    todo: ITodo;
-    onEditHandler: (id: ITodo["id"]) => void;
+  todo: ITodo;
+  onEditHandler: (id: ITodo["id"]) => void;
 }
 
-export const Todo = ({ todo, onEditHandler }: ITodoProps) => {
-    const [editTodo] = useEditTodoMutation();
-    const onChangeCheck = async () => {
-        editTodo({
-            ...todo,
-            completed: !todo.completed,
-        });
-    };
+export const Todo = observer(({ todo, onEditHandler }: ITodoProps) => {
+  const onChangeCheck = async () => {
+    todoStore.update({
+      ...todo,
+      completed: !todo.completed,
+    });
+  };
 
-    return (
-        <div className="todo">
-            <div className="todo__title">
-                <p style={todo.completed ? { textDecoration: "line-through" } : { textDecoration: "none" }}>
-                    {todo.title}
-                </p>
-            </div>
-            <div className="todos__btns">
-                <label htmlFor="check"></label>
-                <input
-                    type="checkbox"
-                    name="check"
-                    id="check"
-                    checked={todo.completed}
-                    onChange={onChangeCheck}
-                />
-                <i
-                    className="fa fa-pencil-square-o"
-                    aria-hidden="true"
-                    onClick={() => onEditHandler(todo.id)}
-                ></i>
-                <DeleteTodo todoId={todo.id} />
-            </div>
-        </div>
-    );
-};
+  return (
+    <div className="todo">
+      <div className="todo__title">
+        <p
+          style={
+            todo.completed
+              ? { textDecoration: "line-through" }
+              : { textDecoration: "none" }
+          }
+        >
+          {todo.title}
+        </p>
+      </div>
+      <div className="todos__btns">
+        <label htmlFor="check"></label>
+        <input
+          type="checkbox"
+          name="check"
+          id="check"
+          checked={todo.completed}
+          onChange={onChangeCheck}
+        />
+        <i
+          className="fa fa-pencil-square-o"
+          aria-hidden="true"
+          onClick={() => onEditHandler(todo.id)}
+        ></i>
+        <DeleteTodo todoId={todo.id} />
+      </div>
+    </div>
+  );
+});
